d438aff0ae121d0ada20e99a31966fd2e80d4878
fififuchun <windkindstar20030205@gmail.com>
2023-09-14 13:33:25 +0900

    データ読み込み前まで

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'pins-quiz';
+//   myForm: FormGroup = this.fb.group({
+//     firstName: [''],
+//     lastName: [''],
+//   });
+//   constructor(private fb: FormBuilder) {}
 }
