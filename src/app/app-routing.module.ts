f00eeefbfad6d60acab28d15e34060ac187fa013
fififuchun <windkindstar20030205@gmail.com>
2023-09-14 10:49:27 +0900

    ルーティングまで

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponent } from './component/first/first.component';
+import { MainComponent } from './component/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/first', pathMatch: 'full' },
+  { path: 'first', component: FirstComponent },
+  { path: 'main', component: MainComponent },
+  { path: '**', redirectTo: '/first', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/shared/header/header.component';
+import { FooterComponent } from './component/shared/footer/footer.component';
+import { FirstComponent } from './component/first/first.component';
+import { MainComponent } from './component/main/main.component';
+import { FilterComponent } from './component/main/filter/filter.component';
+import { QuizComponent } from './component/main/quiz/quiz.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FirstComponent,
+    MainComponent,
+    FilterComponent,
+    QuizComponent
   ],
   imports: [
     BrowserModule,
